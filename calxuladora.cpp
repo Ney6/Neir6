8a118e0730b89ceb848ae8c0bf46ffb03d95eb94
pelotero10 <neil.canchingre.tamayo@utelvt.edu.ec>
2022-09-24 21:33:55 -0500

    calculadora1

---
+++ calxuladora.cpp
@@ -0,0 +1,28 @@
+// Programa que presenta las operaciones basicas
+// Creado por Neil Aldhair Canchingre Tamayo 
+// Fecha: 24-09-2022
+
+# incluir < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal ()
+{
+	flotante x,y,s,m,d,r;
+	// Ingreso de datos
+	cout<< " Ingreso en valor de x=: " ;
+	cin>>x;
+	cout<< " Ingreso en valor de y=: " ;
+	// Operaciones
+	cin>>y;
+	s=x+y;
+	m=x*y;
+	d=x/y;
+	r=xy;
+	// Muestra los resultados.
+	cout<< " Las sumas de " <<x<< " + " <<y<< " = " <<s<<endl;
+	cout<< " Las multiplicaciones de " <<x<< " * " <<y<< " = " <<m<<endl;
+	cout<< " Las divisiones de " <<x<< " / " <<y<< " = " <<d<<endl;
+	cout<< " Las resta de " <<x<< " - " <<y<< " = " <<r<<endl;
+	devolver  0 ;
+
+}
